121cb5e64a7bd1cb62f6bffad29ee99fb410b751
jang-seobeom <hornrimmedglasses0@gmail.com>
2026-01-16 10:26:37 +0000

    chapter04-1 finished

--- python-for-coding-test-practice(book)/chapter04/chapter4-1.py
+++ python-for-coding-test-practice(book)/chapter04/chapter4-1.py
@@ -1,5 +1,5 @@
 input_data = input()
-x = int(input_data[0] - 'a')
+x = int(ord(input_data[0]) - ord('a')) + 1
 y = int(input_data[1])
 
 dx = [2,1,-2,-1,2,1,-2,-1]
@@ -10,7 +10,7 @@ for i in range(8):
     nx = x + dx[i]
     ny = y + dy[i]
 
-    if(nx < 8 and nx >=0 and ny < 8 and ny >=0):
+    if(nx <= 8 and nx >=1 and ny <= 8 and ny >=1):
         count += 1
     
 print(count)
